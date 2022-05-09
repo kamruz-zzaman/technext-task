ff5e563d3cff4edee9a6ad6bb2cf7503f20b516f
kamruz-zzaman <kamruz.zaman8991@gmail.com>
2022-05-10 00:08:35 +0600

    rocket launch details card added

--- src/Components/RocketCard/RocketCard.js
+++ src/Components/RocketCard/RocketCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InputGroup, FormControl, Button } from 'react-bootstrap';
+import { Card, Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
 
 const RocketCard = () => {
     return (
@@ -12,6 +12,24 @@ const RocketCard = () => {
                     />
                     <Button className='ms-5 bg-danger border-danger'>Go</Button>
                 </InputGroup>
+                <div className='container my-5'>
+                    <Row xs={1} md={3} className="g-4 w-100">
+                        {Array.from({ length: 4 }).map((_, idx) => (
+                            <Col >
+                                <Card>
+                                    <Card.Img variant="top" src="holder.js/100px160" />
+                                    <Card.Body>
+                                        <Card.Title>Card title</Card.Title>
+                                        <Card.Text>
+                                            This is a longer card with supporting text below as a natural
+                                            lead-in to additional content. This content is a little bit longer.
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
             </section>
         </>
     );
